e080f12cbeaa6795eb1c5c08648305e2dc4cf2e4
Noah Perlmutter <nperlmutter@decibelinsight.com>
2019-02-21 16:01:35 -0700

    ¯\_(ツ)_/¯

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+	$('#btn-start').on('click', function () {
+		var queryURL = 'http://api.giphy.com/v1/gifs/xUOxeTxr8IruaTTK7u?api_key=dc6zaTOxFJmzC&limit=1';
+
+		$('#hypno-gif').empty();
+
+		$.get(queryURL)
+			.done(function (response) {
+				for (i = 0; i < 250; i++) {
+					var img = $('<img>');
+					//var div = $('<div>');
+					var results = response.data;
+console.log(results);
+
+					img.attr('src', results.images.original.url);
+					img.addClass("sm-gif");
+					//div.append(img);
+					$("#hypno-gif").append(img);
+				}
+			});
+	});
+
+});
